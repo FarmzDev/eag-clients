c41c31afc0b0e8cc73381e2205573a4cd6ff7ebb
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2024-04-17 18:31:42 +0100

    Added 1.5.2 clients (all of them)

--- download.py
+++ download.py
@@ -1,5 +1,16 @@
+import pyperclip
 import requests
+from bs4 import BeautifulSoup
+import zipfile
+import io
 
-response = requests.get("https://archive.eaglercraft.rip/EaglercraftX_1.8/client/u27/EaglercraftX_1.8_Offline_en_US.html")
-with open("download.html", "wb") as file:
-    file.write(response.content)
+response = requests.get("https://archive.eaglercraft.rip/Eaglercraft_1.5/client/")
+soup = BeautifulSoup(response.content, features="lxml")
+versions = [item.text for item in soup.find_all("span", {"class": "name"})]
+versions.reverse()
+output = ""
+for version in versions:
+    version = version.replace("/", "")
+    output += f'\n"{version}": "eagler_1.5.2_{version}",'
+print(output)
+pyperclip.copy(output)
